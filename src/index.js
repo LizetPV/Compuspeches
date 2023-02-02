0e7ffe8bfbb18802643aa6d17f36d77cff7c4327
Lizet Peche <lizet.peche@gmail.com>
2023-02-01 18:18:40 -1100

    proyecto B terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const play = document.getElementById("boton");
+play.addEventListener("click",  () => {
+  const number = document.getElementById("cardnumber").value;
+  const input = document.getElementById("cardnumber");
+  if (number === "") {
+    //Impedir que se ingrese si el campo esta vacio.
+    document.getElementById("texVacio").innerHTML = "Todos los campos son obligatorios ";
+  } else if (number.length > 16) {
+    //Evitar caractereses que no sean digitos de 0 a 9.
+    document.getElementById("texVacio").innerHTML = "debe ingresar 16 digitos como máximo ";
+  } else if (number.length < 16) {
+    //Tener un campo vacio para incertar el numero de tarjeta.
+    document.getElementById("texVacio").innerHTML = "debe ingresar 16 digitos como minimo ";
+  } else {
+    const result = validator.validate(number);
+    if (result === true) {
+      document.getElementById("texVacio").innerHTML = "Numero de tarjeta correcto: " + validator.maskify(number);
+      input.value = ""
+    } else {
+      document.getElementById("texVacio").innerHTML = "El número de tarjeta no es válido " + validator.maskify(number);
+    }
+  }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
-const validator = {
-  // ...
-};
+function validate(cardNumber) {
+  const cadena = cardNumber.toString();
+  const longitud = cadena.length;
+  let cifra = null;
+  let cifra_cad = null;
+  let suma = 0;
+  // aplicacion del algoritmo de luhn
+  for (let i = 0; i < longitud; i += 2) {
+    // bucle que recorre los elementos de la cadena cada dos posiciones
+    cifra = parseInt(cadena.charAt(i)) * 2; // CharAt(i) toma un coracter de la cadena, luego a ese elemento se le convierte a entero y se multiplica por 2
+    if (cifra > 9) {
+      cifra_cad = cifra.toString();
+      cifra = parseInt(cifra_cad.charAt(0)) + parseInt(cifra_cad.charAt(1));
+    }
+    suma = suma + cifra;
+  }
 
+  for (let i = 1; i < longitud; i += 2) {
+    suma = suma + parseInt(cadena.charAt(i));
+  }
+  if (suma % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function maskify(num, n = 4, mask = "#") {
+  const numOculto = num.slice(0, -n).replace(/./g, mask) + num.slice(-n);
+  return numOculto;
+}
+const validator = {validate,maskify};
 export default validator;
+
+
